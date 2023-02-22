1c32742c45fa98ec26b3b0121cdf2a34e59accd7
Salikh <pernebek.sm@gmail.com>
2023-02-22 23:20:45 +0600

    fields field from appointments index view has been removed

--- resources/views/appointments/index.blade.php
+++ resources/views/appointments/index.blade.php
@@ -14,9 +14,12 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div>
+                        <div class="mb-5 text-lg">
+                            {{ __("Use search field below to find necessary service and make an appointment:") }}
+                        </div>
                         <div class="inline-flex flex-row">
                             <form action="{{ route('appointments.search') }}" method="get" accept-charset="utf-8">
-                                <input type="text" class="border-black rounded-2xl" name="search_name" style="width: 600px" placeholder="Enter the doctor's name">
+                                <input type="text" class="border-black rounded-2xl" name="search_name" style="width: 600px" placeholder="Enter the doctor's name or service">
                                 <button type="submit" class="ml-8 inline-block bg-teal-400 rounded-2xl" style="padding: 10px 20px">Search</button>
                             </form>
                         </div>
@@ -32,7 +35,7 @@
     </div>
 
 
-        <div class="py-12" style="margin-top: -70px">
+        {{--<div class="py-12" style="margin-top: -70px">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
@@ -42,14 +45,14 @@
 
                         <div class="inline-grid grid-cols-3 gap-4">
                             @foreach($fields as $field)
-                                {{-- modal activation --}}
+                                --}}{{-- modal activation --}}{{--
 
                                 <button data-modal-target="{{ $field->field }}" data-modal-toggle="{{ $field->field }}" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
                                     {{ $field->field }}
                                 </button>
 
-                                {{-- modal itself --}}
-                                {{--<div id="{{ $field->field }}" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+                                --}}{{-- modal itself --}}{{--
+                                --}}{{--<div id="{{ $field->field }}" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
                                     <div class="relative w-full h-full max-w-2xl md:h-auto">
                                         <!-- Modal content -->
                                         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -78,8 +81,8 @@
                                             </div>
                                         </div>
                                     </div>
-                                </div>--}}
-                                {{--<a href="#" class="text-lg text-blue-600 mr-4 hover:underline md:mr-6">{{ $field->field }}</a>--}}
+                                </div>--}}{{--
+                                --}}{{--<a href="#" class="text-lg text-blue-600 mr-4 hover:underline md:mr-6">{{ $field->field }}</a>--}}{{--
                             @endforeach
 
                         </div>
@@ -89,7 +92,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div>--}}
 
 
     <div class="py-12" style="margin-top: -70px">
