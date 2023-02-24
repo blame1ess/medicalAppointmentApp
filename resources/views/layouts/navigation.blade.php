08d2f0a053e4c6ed474034352cfc6709e639b09f
Salikh <pernebek.sm@gmail.com>
2023-02-25 01:31:14 +0600

    navigation menu responssive links has been changed

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -105,21 +105,59 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('appointments')" :active="request()->routeIs('appointments')">
-                {{ __('Make an Appointment') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('personal_data')" :active="request()->routeIs('appointments')">
-                {{ __('Personal Data') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('faq')" :active="request()->routeIs('appointments')">
-                {{ __('FAQ') }}
-            </x-responsive-nav-link>
 
-        </div>
+        @if(\Illuminate\Support\Facades\Auth::user()->user_type == 'patient')
+
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    {{ __('Dashboard') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('appointments')" :active="request()->routeIs('appointments')">
+                    {{ __('Make an Appointment') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('personal_data')" :active="request()->routeIs('appointments')">
+                    {{ __('Personal Data') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('faq')" :active="request()->routeIs('appointments')">
+                    {{ __('FAQ') }}
+                </x-responsive-nav-link>
+
+            </div>
+
+        @elseif(\Illuminate\Support\Facades\Auth::user()->user_type == 'admin')
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    {{ __('Dashboard') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('admin.appointments')" :active="request()->routeIs('admin.appointments')">
+                    {{ __('Manage Appointments') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('manage_staff')" :active="request()->routeIs('manage_staff')">
+                    {{ __('Manage Staff') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('faq')" :active="request()->routeIs('appointments')">
+                    {{ __('Patients data') }}
+                </x-responsive-nav-link>
+
+            </div>
+
+        @else
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    {{ __('Dashboard') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('appointments')" :active="request()->routeIs('appointments')">
+                    {{ __('Make an Appointment') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('personal_data')" :active="request()->routeIs('appointments')">
+                    {{ __('Personal Data') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('faq')" :active="request()->routeIs('appointments')">
+                    {{ __('FAQ') }}
+                </x-responsive-nav-link>
+
+            </div>
+        @endif
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
